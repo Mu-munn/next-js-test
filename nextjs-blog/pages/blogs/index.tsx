a47f1b59876f7d8a5ed2280a13b0ec08bbc8f425
Mu-munn <rpgmukai333@gmail.com>
2022-01-19 12:17:49 +0900

    WORKSページの装　

--- nextjs-blog/pages/blogs/index.tsx
+++ nextjs-blog/pages/blogs/index.tsx
@@ -1,33 +1,62 @@
 import { NextPage, InferGetStaticPropsType } from 'next';
-import { Box } from '@chakra-ui/react';
+import { Box, Center, Flex, Heading, VStack } from '@chakra-ui/react';
 import { getAllPosts } from '../../utils/api';
+import AppBar from '../../components/appBar';
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 export const getStaticProps = async () => {
-  const allPosts = getAllPosts(['slug', 'title', 'date', 'tags']);
+    const allPosts = getAllPosts(['slug', 'title', 'date', 'tags']);
 
-  return {
-    props: { allPosts },
-  };
+    return {
+        props: { allPosts },
+    };
 };
 
 const Home: NextPage<Props> = ({ allPosts }) => (
-  <ul>
-    {allPosts?.map((post) => (
-      <Box key={post.slug} mt={10}>
-        <li>
-          <h2>{post.title}</h2>
-          <p>{post.date}</p>
-          <ul>
-            {post.tags?.map((tag) => (
-              <li key={tag}>{tag}</li>
-            ))}
-          </ul>
-        </li>
-      </Box>
-    ))}
-  </ul>
+    <Box>
+        <Box Box bg="gray.100" w="full" h="1000vh" >
+            <AppBar />
+            <Center w="100%" bg="" mt="10%" >
+                <Box w="80%">
+                    <Flex justifyContent="space-around" wrap="wrap">
+                        {allPosts?.map((post) => (
+                            <Box key={post.slug} mt={10} mr="20px" bg="white" w="300px" h="300px" boxShadow='xl' p='9' rounded='md' >
+                                <VStack>
+                                    <h2>{post.title}</h2>
+                                    <p>{post.date}</p>
+                                    <ul>
+                                        {post.tags?.map((tag) => (
+                                            <li key={tag}>{tag}</li>
+                                        ))}
+                                    </ul>
+                                </VStack>
+                            </Box>
+                        ))}
+                    </Flex>
+                </Box>
+
+            </Center>
+        </Box>
+
+    </Box>
+
+    //     <Box>
+    //     {allPosts?.map((post) => (
+    //       <Box key={post.slug} mt={10}bg="white" w="400px" h="400px" boxShadow='xl' p='9' rounded='md' >
+    //         <VStack>
+    //           <h2>{post.title}</h2>
+    //           <p>{post.date}</p>
+    //           <ul>
+    //             {post.tags?.map((tag) => (
+    //               <li key={tag}>{tag}</li>
+    //             ))}
+    //           </ul>
+    //         </VStack>
+    //       </Box>
+    //     ))}
+    //   </Box>
+
 );
 
 export default Home;
